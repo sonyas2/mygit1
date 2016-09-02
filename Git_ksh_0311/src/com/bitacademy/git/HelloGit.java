b7206369e9e5a7f97bfcccf1a4031d3d5407579a
sonya <sonias2@naver.com>
2016-09-02 11:46:37 +0900

    프로젝트 공유, git으로

---
+++ Git_ksh_0311/src/com/bitacademy/git/HelloGit.java
@@ -0,0 +1,11 @@
+package com.bitacademy.git;
+
+public class HelloGit {
+
+	public static void main(String[] args) {
+		System.out.println("¾È³ç Git");
+		System.out.println("Á¹·Á¾î¾î¾î¾î ¹ä¸Ô±¸ Ä¿ÇÇ»ç¾ß°Ô½á¤¾¤¾");
+
+	}
+
+}
