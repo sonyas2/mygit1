170053cd4eb76af09d93d2e88483640918d49d02
sonya <sonias2@naver.com>
2016-09-02 13:17:30 +0900

    123 파일 수정

--- Git_ksh_0311/src/com/bitacademy/git/HelloGit.java
+++ Git_ksh_0311/src/com/bitacademy/git/HelloGit.java
@@ -5,7 +5,9 @@ public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("안녕 Git");
 		System.out.println("졸려어어어어 밥먹구 커피사야게써ㅎㅎ");
-
+		//아메리카노 좋아~
+		//아메아메아메
+		//시럽 빼고주세요
 	}
 
 }
